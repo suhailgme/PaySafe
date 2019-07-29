a72f4677fc07ae9b762809fb96ccdb3455e9731f
SuhailG <zonyc17@msn.com>
2019-07-29 00:43:18 -0600

    Add NavBar Component

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {
+    Flex,
+    Box,
+    Heading
+} from 'rimble-ui'
+import NetworkIndicator from '@rimble/network-indicator'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <Flex p={3} className='navbar'>
+                <Box p={2} width={1 / 2}>
+                    <Heading color={'blue'}>PaySafe</Heading>
+                </Box>
+                <Box width={1 / 2}>
+                    <NetworkIndicator currentNetwork={this.props.networkId} requiredNetwork={4} />
+                    {/* <Text fontSize={1} fontWeight={4}>Account: {this.state.accounts}</Text> */}
+                </Box>
+            </Flex>
+        )
+    }
+}
