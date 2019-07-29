6e3fa4b5e13461072a9b8eb3e8153a4ccb0e400d
SuhailG <zonyc17@msn.com>
2019-07-29 00:44:52 -0600

    Add PaySafe.sol tests for several functions

---
+++ test/paysafe.js
@@ -0,0 +1,67 @@
+const PaySafe = artifacts.require("./PaySafe.sol");
+const catchRevert = require('./exceptions').catchRevert
+const BN = web3.utils.BN
+
+contract("PaySafe", accounts => {
+    const deployer = accounts[0]
+    const firstAccount = accounts[1]
+    const secondAccount = accounts[2]
+    const thirdAccount = accounts[3]
+    const fourthAccount = accounts[4]
+    const oneEth = 1000000000000000000
+    let instance
+
+    beforeEach(async () => {
+        instance = await PaySafe.new()
+    })
+    console.log(`Deployer account: ${deployer}`)
+    console.log(`firstAccount account: ${firstAccount}`)
+    console.log(`secondAccount account: ${secondAccount}`)
+    console.log(`thirdAccount account: ${thirdAccount}`)
+
+
+    describe("Contract Ownership", async () => {
+        it("Contract owner should be set to deploying address", async () => {
+            let owner = await instance.owner()
+            assert.equal(deployer, owner, "Deploying address should be owner")
+        })
+        it("Only the Contract owner should be able to transfer ownership", async () => {
+            await catchRevert(instance.transferOwnership(firstAccount, { from: secondAccount }))
+        })
+        it("The Contract owner should be able to transfer ownership", async () => {
+            await instance.transferOwnership(firstAccount)
+            let newOwner = await instance.owner()
+            assert.equal(firstAccount, newOwner, `${firstAccount} should be owner`)
+        })
+    })
+
+    describe("New Transaction", async () => {
+        it("Creates a new transaction", async () => {
+            let firstTransactionId = await instance.newTransaction(secondAccount, "0x0", { from: firstAccount, value: oneEth })
+            firstTransactionId = firstTransactionId.logs[0].args.transactionId.toString()
+            let secondTransactionId = await instance.newTransaction(thirdAccount, "0x0", { from: firstAccount, value: oneEth })
+            secondTransactionId = secondTransactionId.logs[0].args.transactionId.toString()
+            assert.equal(firstTransactionId, 0, `transaction ID should be 0`)
+            assert.equal(secondTransactionId, 1, `transaction ID should be 1`)
+
+        })
+        it("Has the correct transaction details", async () => {
+            let transactionId = await instance.newTransaction(secondAccount, "0x0", { from: firstAccount, value: oneEth })
+            transactionId = transactionId.logs[0].args.transactionId.toString()
+            let currentTransaction = await instance.getTransaction(transactionId)
+            assert.equal(currentTransaction.source, firstAccount, `source should be${firstAccount}`)
+            assert.equal(currentTransaction.destination, secondAccount, `destination should be ${secondAccount}`)
+            assert.equal(currentTransaction.value.toString(), oneEth, `value should be ${oneEth}`)
+        })
+    })
+    describe("Withdraw", async () => {
+        it("Allows the recipient to withdraw funds", async () => {
+            let beforeBalance = await web3.eth.getBalance(thirdAccount)
+            let transactionId = await instance.newTransaction(thirdAccount, "0x0", { from: firstAccount, value: oneEth })
+            transactionId = transactionId.logs[0].args.transactionId.toString()
+            await instance.withdraw(transactionId, {from: thirdAccount})
+            let afterBalance = await web3.eth.getBalance(thirdAccount)
+            assert.isAbove(parseInt(afterBalance),parseInt(beforeBalance) , `Balance should be greater than 100ETH`)
+        })
+    })
+})
