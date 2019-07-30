f7d33ad605d6753d4d5e99b69df42e40842792be
SuhailG <zonyc17@msn.com>
2019-07-30 02:16:31 -0600

    Update network indicator to work on custom networks

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -14,7 +14,10 @@ export default class NavBar extends Component {
                     <Heading color={'blue'}>PaySafe</Heading>
                 </Box>
                 <Box width={1 / 2}>
-                    <NetworkIndicator currentNetwork={this.props.networkId} requiredNetwork={4} />
+                    {this.props.networkId === 5777 ?
+                        <NetworkIndicator currentNetwork={this.props.networkId} requiredNetwork={5777} /> :
+                        <NetworkIndicator currentNetwork={this.props.networkId} requiredNetwork={4} />
+                    }
                     {/* <Text fontSize={1} fontWeight={4}>Account: {this.state.accounts}</Text> */}
                 </Box>
             </Flex>
