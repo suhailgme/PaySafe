3558dd16b6b1c8452db615d50f0ad0d78688c179
SuhailG <zonyc17@msn.com>
2019-07-30 02:18:07 -0600

    Split up components to clean up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
-import ControlledSendContract from "./contracts/ControlledSend.json";
+import PaySafeContract from "./contracts/PaySafe.json";
 import getWeb3 from "./utils/getWeb3";
-
+import NavBar from './components/NavBar'
+import NewPayment from './components/NewPayment'
+import AccountInfo from './components/AccountInfo'
+import Payments from './components/Payments'
+import {
+  Flex,
+  Box,
+} from 'rimble-ui'
 import "./App.css";
+import { async } from "q";
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -17,15 +25,17 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = ControlledSendContract.networks[networkId];
+      const deployedNetwork = PaySafeContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        ControlledSendContract.abi,
+        PaySafeContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
+      let balance = await web3.eth.getBalance(accounts[0])
+      balance = web3.utils.fromWei(balance, 'ether')
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, networkId, balance, contract: instance, deployedNetwork, });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -34,6 +44,14 @@ class App extends Component {
       console.error(error);
     }
   };
+
+  updateBalance = async () => {
+    const web3 = this.state.web3
+    const account = this.state.accounts[0]
+    let balance = await web3.eth.getBalance(account)
+    balance = web3.utils.fromWei(balance, 'ether')
+    this.setState({ balance })
+  }
 
   // runExample = async () => {
   //   const { accounts, contract } = this.state;
@@ -51,22 +69,39 @@ class App extends Component {
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
+    } else {
+      const { accounts, balance, networkId } = this.state
+      return (
+        <div className="App">
+          <NavBar networkId={networkId} />
+          <Flex style={{ paddingTop: '50px' }} flexWrap='wrap'>
+            <Box width={[1, 1, 0.5]} >
+              <NewPayment
+                account={accounts[0]}
+                web3={this.state.web3}
+                contract={this.state.contract}
+                balance={balance}
+                updateBalance={this.updateBalance}
+              />
+            </Box>
+            <Box width={[1, 1, 0.5]} >
+              <AccountInfo account={accounts[0]} balance={balance} />
+            </Box>
+          </Flex>
+          <Flex pt={4}>
+            <Payments
+              account={accounts[0]}
+              web3={this.state.web3}
+              contract={this.state.contract}
+              updateBalance={this.updateBalance}
+            />
+          </Flex>
+
+        </div>
+      )
     }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
+
+
   }
 }
 
