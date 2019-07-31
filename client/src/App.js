1f4fa70ff02bb639f09bf2d8c9199d339a4d5a30
SuhailG <zonyc17@msn.com>
2019-07-30 22:25:11 -0600

    Remove example function

--- client/src/App.js
+++ client/src/App.js
@@ -53,19 +53,6 @@ class App extends Component {
     this.setState({ balance })
   }
 
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
-
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(5).send({ from: accounts[0] });
-
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
-
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
-
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
